6eac22378e01f713e6b399715b38954b8f6f2e92
alexazarh <aazarh@gmail.com>
2018-12-16 11:00:59 +0200

    added script that creates an app with permissions for cloudshell integration with azure

---
+++ api_setup.sh
@@ -0,0 +1,103 @@
+#!/bin/bash
+export AZURE_HTTP_USER_AGENT='pid-5a91ff6a-52b4-4e6e-92c8-1f47c8e9b9c6'
+
+function quit_on_err {
+	echo $1			  			  
+	exit 
+}
+		   
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+NC='\033[0m' # No Color
+REGION="eastus"
+
+if [ ! -z "$1" ]
+then
+      REGION=$1
+fi
+
+echo -e "\n\n   ___ _    ___  _   _ ___  ___ _  _ ___ __"
+echo -e "  / __| |  / _ \| | | |   \/ __| || | __| |  | |  "
+echo -e " | (__| |_| (_) | |_| | |) \__ \ __ | _|| |__| |__"
+echo -e "  \___|____\___/ \___/|___/|___/_||_|___|____|____|\n\n\n"
+
+
+echo -e "$GREEN Please wait while we setup the integration with your Azure account. $NC \n\n"
+echo -e "This script grants Cloud Shell permissions to your account."
+#========================================================================================
+x=$(az account list)
+accountname=$(az account show |jq -r .user.name)
+length=$(jq -n "$x" | jq '. | length')
+END=$length-1
+i=0
+declare -i subscription_number=0
+
+if [ "$length" -eq 0 ]
+then
+        echo -e "${RED}Error no subscription found${NC}"
+        exit 1
+fi
+
+if [ "$length" -eq 1 ]
+then
+        subscription_number=1
+else 
+        echo "Please type subscription number:#"
+        while [[ $i -le $END ]]
+        do
+                # prints subscription name and id
+                echo "$((i+1))" $(jq -n "$x" | jq .["$i"].name )  $(jq -n "$x" | jq .["$i"].id )
+                ((i++))
+        done
+
+        read -p  "Please enter number between 1 to $length: " subscription_number
+
+        while [  $subscription_number -lt 1 -o $subscription_number -gt $length ]
+        do	
+                read -p "Please enter number between 1 to $length: " subscription_number
+        done
+fi
+
+SubscriptionId=$(jq -n "$x" | jq .["$((subscription_number-1))"].id -r)
+
+echo -e "Running with settings:"
+echo -e "Subscription:" $GREEN$(jq -n "$x" | jq .["$((subscription_number-1))"].name )  $SubscriptionId$NC
+echo -e "Region $GREEN$REGION$NC"
+
+#========================================================================================
+
+
+
+#creating a random key
+CLOUDSHELL_RANDOM=$(date +%s | sha256sum | base64 | head -c 12;echo)$(echo $RANDOM)
+CLOUDSHELL_RANDOM="$(echo $CLOUDSHELL_RANDOM | tr '[A-Z]' '[a-z]')"
+AppName=$(echo "CLOUDSHELL-"$CLOUDSHELL_RANDOM)
+AppKey=$(openssl rand -base64 32)
+TenantId=$(az account show --query tenantId -o tsv)
+
+
+echo -e "Creating AD application for CloudShell"
+az ad sp create-for-rbac -n $AppName --password $AppKey --subscription $SubscriptionId ||  quit_on_err "The user that runs the script should be an Owner."
+
+
+AppId=$(az ad app list --subscription $SubscriptionId --display-name $AppName | jq '.[0].appId' | tr -d \")
+
+echo -e "Configuring access to Azure API"
+bash -c "cat >> role.json" <<EOL
+[{"resourceAppId": "797f4846-ba00-4fd7-ba43-dac1f8f63013","resourceAccess":[{"id": "41094075-9dad-400e-a0bd-54e686782033", "type":"Scope"}]}]
+EOL
+ 
+az ad app update --id $AppId --required-resource-accesses role.json --subscription $SubscriptionId
+rm role.json
+echo -e "\n\nApplication Name : $AppName \nApplication ID : $AppId \nApplication Key : $AppKey \nTenant ID : $TenantId \nSubscription ID : $SubscriptionId"
+
+#========================================================================================
+
+
+echo -e "\n\n\n-------------------------------------------------------------------------"
+echo "Copy the values below and paste it into the relevant attributes of your CloudShell's Azure cloud provider resource"
+echo -e "${GREEN}nApplication : $AppId \nApplication Key : $AppKey \nTenant ID : $TenantId \nSubscription ID:$SubscriptionId"
+echo -e "-------------------------------------------------------------------------\n\n"
+
+                                                                                     
+echo "Done"
